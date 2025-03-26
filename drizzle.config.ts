20cd2c64646932715a06be55e9258e177574f3f8
Yusuf <mdyusuf2521@gmail.com>
2025-03-26 13:08:59 +0530

    Added dotenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+console.log(process.env.DATABASE_URL);
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
