a3616bb7402116ca45324ebf2bbb017f2a7a80a4
Yusuf <mdyusuf2521@gmail.com>
2025-04-13 14:22:24 +0530

    added console

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { AuthProvider } from "@/hooks/use-auth";
+import { BACKEND_URL } from "./lib/constants";
 import { Toaster } from "@/components/ui/toaster";
 import { ProtectedRoute } from "@/lib/protected-route";
 import Footer from "@/components/footer";
@@ -46,8 +47,9 @@ export default function App() {
   //   if (isValidating) return <Loader />;
   // }
   
-
+console.log("Bcakend Url", BACKEND_URL);
   return (
+
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <div className="min-h-screen bg-background flex flex-col">
